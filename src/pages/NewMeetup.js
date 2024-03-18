fb72c95ff1cd06575c8427acbc7d5b2c5e22368b
Sarah Roperch <sarah.roperch@nexelec.fr>
2024-03-18 15:26:58 +0100

    demo finished

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,4 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
-  return <div>New Meetup Page</div>;
+  const navigate = useNavigate();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started2-2c1ef-default-rtdb.europe-west1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: { "Content-Type": "application/json" },
+      }
+    ).then(() => {
+      navigate("/");
+    });
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
+
 export default NewMeetupPage;
